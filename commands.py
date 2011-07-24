9bb174a2f24a3c6dc9a8e44dc6a8c539d68182dd
Igor Popov <ihptru@gmail.com>
2011-07-25 03:05:25 +0400

    ]randomteam: pyrand is a module now

--- commands.py
+++ commands.py
@@ -8,6 +8,8 @@ import pywapi
 import urllib.request
 import time
 
+import pyrand
+
 languages=['af','sq','ar','be','bg','ca','zh-CN','hr','cs','da','nl','en','et','tl','fi','fr','gl','de','el','iw','hi','hu','is','id','ga','it','ja','ko','lv','lt','mk','ml','mt','no','fa','pl','ro','ru','sr','sk','sl','es','sw','sv','th','tr','uk','vi','cy','yi']
 real_langs=['Afrikaans','Albanian','Arabic','Belarusian','Bulgarian','Catalan','Chinese_Simplified','Croatian','Czech','Danish','Dutch','English','Estonian','Filipino','Finnish','French','Galician','German','Greek','Hebrew','Hindi','Hungarian','Icelandic','Indonesian','Irish','Italian','Japanese','Korean','Latvian','Lithuanian','Macedonian','Malay','Maltese','Norwegian','Persian','Polish','Romanian','Russian','Serbian','Slovak','Slovenian','Spanish','Swahili','Swedish','Thai','Turkish','Ukrainian','Vietnamese','Welsh','Yiddish']
 codes=['AF','AX','AL','DZ','AS','AD','AO','AI','AQ','AG','AR','AM','AW','AU','AT','AZ','BS','BH','BD','BB','BY','BE','BZ','BJ','BM','BT','BO','BQ','BA','BW','BV','BR','IO','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CN','CX','CC','CO','KM','CG','CD','CK','CR','CI','HR','CU','CW','CY','CZ','DK','DJ','DM','DO','EC','EG','SV','GQ','ER','EE','ET','FK','FO','FJ','FI','FR','GF','PF','TF','GA','GM','GE','DE','GH','GI','GR','GL','GD','GP','GU','GT','GG','GN','GW','GY','HT','HM','VA','HN','HK','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','JM','JP','JE','JO','KZ','KE','KI','KP','KR','KW','KG','LA','LV','LB','LS','LR','LY','LI','LT','LU','MO','MK','MG','MW','MY','MV','ML','MT','MH','MQ','MR','MU','YT','MX','FM','MD','MC','MN','ME','MS','MA','MZ','MM','NA','NR','NP','NL','NC','NZ','NI','NE','NG','NU','NF','MP','NO','OM','PK','PW','PS','PA','PG','PY','PE','PH','PN','PL','PT','PR','QA','RE','RO','RU','RW','BL','SH','KN','LC','MF','PM','VC','WS','SM','ST','SA','SN','RS','SC','SL','SG','SX','SK','SI','SB','SO','ZA','GS','ES','LK','SD','SR','SJ','SZ','SE','CH','SY','TW','TJ','TZ','TH','TL','TG','TK','TO','TT','TN','TR','TM','TC','TV','UG','UA','AE','GB','US','UM','UY','UZ','VU','VE','VN','VG','VI','WF','EH','YE','ZM','ZW']
@@ -455,16 +457,7 @@ def randomteam(self, user, channel):
     command = (self.command)
     command = command.split()
     if ( len(command) > 3 ):
-        team_names = " ".join(command[1:])
-        os.system("python ../pyrand.py "+team_names)
-        filename = 'pyrand.txt'
-        file = open(filename, 'r')
-        result_pyrand = file.readline()
-        file.close()
-        if re.search("^#", channel):
-            self.send_message_to_channel( (result_pyrand.rstrip()), channel)
-        else:
-            self.send_message_to_channel( (result_pyrand.rstrip()), user)
+        pyrand.start(self, user, channel, command[1:])
     else:
         if re.search("^#", channel):
             self.send_message_to_channel( ("You must specify at least 3 teams"), channel)
--- pyrand.py
+++ pyrand.py
@@ -1,16 +1,19 @@
-#!/bin/python
-# -*- coding: utf-8 -*-
-# filename: pyrand.py 
-#to run: python pyrand.py team_1 team2 team3 team_4
 #no spaces in team names
 import re
 from sys import stdout, argv
 from os import stat
 from time import sleep, strftime, gmtime, localtime
 import random
-output_name='/var/sites/lv-vl/randomteam/'+strftime('%y%m%d%H%M%S',localtime())+'.txt'
-output_file=output_name
-output_name = output_name.split('randomteam/')[1]
+
+output_file = ''
+
+def start(self, user, channel, players):
+    output_name='/var/sites/lv-vl/randomteam/'+strftime('%y%m%d%H%M%S',localtime())+'.txt'
+    global output_file
+    output_file=output_name
+    output_name = output_name.split('randomteam/')[1]
+    rand(self, user, channel, players, output_name)
+    
 def o_writter(text):
     #stdout.write(text)
     #stdout.flush()
@@ -26,82 +29,75 @@ def qsortRange(a, b, start, end):
         qsortRange(a, pivotIndex + 1, end)
     return a
 
-# todo teams64=["01","64"]
-#       [1,02,3  4  5  6  7 8 9 10 11 12 13 14 15 1617 1819 20 21 22 23 242526 27 28 29 30 31 32]
-teams32=[1,32,17,16,9,24,25,8,5,28,21,12,13,20,29,4,3,30,19,14,11,22,27,6,7,26,23,10,15,18,31,2]
-teams16=[1,15,11,7,5,9,13,3,4,14,10,6,8,12,16,2]
-teams8=[1,7,5,3,4,6,8,2]
-teams4=[1,4,3,2]
+def rand(self, user, channel, players, output_name):
+    # todo teams64=["01","64"]
+    #       [1,02,3  4  5  6  7 8 9 10 11 12 13 14 15 1617 1819 20 21 22 23 242526 27 28 29 30 31 32]
+    teams32=[1,32,17,16,9,24,25,8,5,28,21,12,13,20,29,4,3,30,19,14,11,22,27,6,7,26,23,10,15,18,31,2]
+    teams16=[1,15,11,7,5,9,13,3,4,14,10,6,8,12,16,2]
+    teams8=[1,7,5,3,4,6,8,2]
+    teams4=[1,4,3,2]
 
+    numTeams=len(players)
 
-players=[]
-first=True;
-for arg in argv:
-    if first:
-        first=False
-    else:
-        players.append((arg))
-numTeams=len(players)
-
-for i in range(0,len(players)):
-    x = random.randint(i,(len(players)-1))
-    temp = players[i]
-    players[i] = players[x]
-    players[x] = temp
+    for i in range(0,len(players)):
+        x = random.randint(i,(len(players)-1))
+        temp = players[i]
+        players[i] = players[x]
+        players[x] = temp
     
-for i in range(0,len(players)):
-    x = random.randint(i,(len(players)-1))
-    temp = players[i]
-    players[i] = players[x]
-    players[x] = temp
-byenum=1
-if (numTeams<=4 and numTeams>0):
-    teams=teams4
-    while (numTeams<4):
-        players.append("~bye~"+str(byenum))
-        byenum=byenum+1
-        numTeams=len(players)
-if (numTeams<=8 and numTeams>4):
-    teams=teams8
-    while (numTeams<8):
-        players.append("~bye~"+str(byenum))
-        byenum=byenum+1
-        numTeams=len(players)
-if (numTeams<=16 and numTeams>8):
-    teams=teams16
-    while (numTeams<16):
-        players.append("~bye~"+str(byenum))
-        byenum=byenum+1
-        numTeams=len(players)
-if (numTeams<=32 and numTeams>16):
-    teams=teams32
-    while (numTeams<32):
-        players.append("~bye~"+str(byenum))
-        byenum=byenum+1
-        numTeams=len(players)
+    for i in range(0,len(players)):
+        x = random.randint(i,(len(players)-1))
+        temp = players[i]
+        players[i] = players[x]
+        players[x] = temp
+    byenum=1
+    if (numTeams<=4 and numTeams>0):
+        teams=teams4
+        while (numTeams<4):
+            players.append("~bye~"+str(byenum))
+            byenum=byenum+1
+            numTeams=len(players)
+    if (numTeams<=8 and numTeams>4):
+        teams=teams8
+        while (numTeams<8):
+            players.append("~bye~"+str(byenum))
+            byenum=byenum+1
+            numTeams=len(players)
+    if (numTeams<=16 and numTeams>8):
+        teams=teams16
+        while (numTeams<16):
+            players.append("~bye~"+str(byenum))
+            byenum=byenum+1
+            numTeams=len(players)
+    if (numTeams<=32 and numTeams>16):
+        teams=teams32
+        while (numTeams<32):
+            players.append("~bye~"+str(byenum))
+            byenum=byenum+1
+            numTeams=len(players)
 
-players2=[]
-for i in range(0,numTeams):
-    players2.append(" ")
-i=0
-for indexVal in teams:
-    players2.pop(indexVal-1)
-    players2.insert(indexVal-1, players[i])
-    i=i+1
-playerList=""
-for i in range(0,len(players2)):
-    if (i <9):
-        o_writter("| RD1-team0"+str(i+1)+"="+players2[i]+"\n")
-        playerList=playerList+str(players2[i])+", "
-    else:
-        o_writter("| RD1-team"+str(i+1)+"="+players2[i]+"\n")
-        if(i is (len(players2)-1)):
-            playerList=playerList+str(players2[i])
-        else:
+    players2=[]
+    for i in range(0,numTeams):
+        players2.append(" ")
+    i=0
+    for indexVal in teams:
+        players2.pop(indexVal-1)
+        players2.insert(indexVal-1, players[i])
+        i=i+1
+    playerList=""
+    for i in range(0,len(players2)):
+        if (i <9):
+            o_writter("| RD1-team0"+str(i+1)+"="+players2[i]+"\n")
             playerList=playerList+str(players2[i])+", "
+        else:
+            o_writter("| RD1-team"+str(i+1)+"="+players2[i]+"\n")
+            if(i is (len(players2)-1)):
+                playerList=playerList+str(players2[i])
+            else:
+                playerList=playerList+str(players2[i])+", "
             
-result_to_file = "http://lv-vl.net/randomteam/"+output_name+"   "+playerList
-filename = 'pyrand.txt'
-file = open(filename, 'w')
-file.write(result_to_file)
-file.close()
+    result = "http://lv-vl.net/randomteam/"+output_name+"   "+playerList
+    if re.search("^#", channel):
+        self.send_message_to_channel( (result), channel)
+    else:
+        self.send_message_to_channel( (result), user)
