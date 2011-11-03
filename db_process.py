e2843ab0b0dd9823204c5488c8975985a82df0d7
Igor Popov <ihptru@gmail.com>
2011-11-03 20:18:41 +0300

    remove unneeded import

--- db_process.py
+++ db_process.py
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sqlite3
-
 def start(self):
     conn, cur = self.db_data()
     print(("[%s] Creating databases") % (self.irc_host))
