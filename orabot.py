d56fb1fe234a0024ffe551d57dff12315e76a52b
Igor Popov <ihptru@gmail.com>
2011-11-25 20:17:56 +0300

    restriction of bot's message's size still must be

--- orabot.py
+++ orabot.py
@@ -271,7 +271,7 @@ class IRC_Server:
     # This function sends a message to a channel or user
     def send_message_to_channel(self, data, channel):
         print ( ( "[%s] %s: %s") % (self.irc_host, self.irc_nick, data) )
-        self.irc_sock.send( (("PRIVMSG %s :%s\r\n") % (channel, data)).encode() )
+        self.irc_sock.send( (("PRIVMSG %s :%s\r\n") % (channel, data[0:512])).encode() )
         time.sleep(1)
         ### logs
         self.logs(self.irc_nick, channel, 'privmsg', str(data), '')
