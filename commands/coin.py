c53d64c18c122af0de75e75d1f6f898c6ea226a3
ihptru <ihptru@gmail.com>
2016-04-08 11:21:51 +0300

    fixes #79;

---
+++ commands/coin.py
@@ -0,0 +1,26 @@
+# Copyright 2011-2016 orabot Developers
+#
+# This file is part of orabot, which is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Simple coin toss
+"""
+
+import random
+
+def coin(self, user, channel):
+    command = (self.command).split()
+    if ( len(command) == 1 ):
+        self.send_reply( (random.choice(['head', 'tail'])), user, channel )
+        return True
